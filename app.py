c906a8f3397e4fb2a84b9b7299d3ac59750a1aaa
Sia <babak.barghi@gmail.com>
2024-04-04 21:59:43 +0200

    setup the env

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+
+LANGCHAIN_TRACING_V2=true
+LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
+LANGCHAIN_API_KEY="<your-api-key>"
+LANGCHAIN_PROJECT="OrixaAI"
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = "LANGCHAIN_API_KEY"
