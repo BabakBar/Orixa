32d4dd95a7f6434d2d642dc2c9681fbdc0aab9e3
Sia <babak.barghi@gmail.com>
2024-04-04 22:36:49 +0200

    langsmith tracing

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
-LANGCHAIN_TRACING_V2="true"
-LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
-LANGCHAIN_API_KEY="<your-api-key>"
-LANGCHAIN_PROJECT="OrixaAI"
-
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = "LANGCHAIN_API_KEY"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
 
 # Define a prompt template for conversation
 prompt = ChatPromptTemplate.from_messages([
