3073a85840b41638cfc318e99893d5652c96842f
Sia <babak.barghi@gmail.com>
2024-04-04 21:49:20 +0200

    import models

---
+++ app.py
@@ -0,0 +1,8 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+load_dotenv()
