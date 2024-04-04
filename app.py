aa9bebff038afa8245938ba5845ce9f13d0a4b9c
Sia <babak.barghi@gmail.com>
2024-04-04 22:08:48 +0200

    define llm

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-LANGCHAIN_TRACING_V2=true
+LANGCHAIN_TRACING_V2="true"
 LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
 LANGCHAIN_API_KEY="<your-api-key>"
 LANGCHAIN_PROJECT="OrixaAI"
@@ -16,3 +16,22 @@ LANGCHAIN_PROJECT="OrixaAI"
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = "LANGCHAIN_API_KEY"
+
+# Define a prompt template for conversation
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant. Please respond to the user's query."),
+    ("user", "Question:{question}")
+])
+
+# Set up the Streamlit interface with a title and input field
+st.title('Orixa with Langchain')
+input_text = st.text_input("Search the topic you want")
+
+# openai llm
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    response = chain.invoke({'question': input_text})
+    st.write(response)
