3cd51236c423a34aa0ad048f7a6990c30d880d63
Babak <babak.barghi@conti.de>
2024-04-08 16:23:38 +0200

    import AgentType

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
 import pandas as pd
 
 import streamlit as st
@@ -28,6 +29,16 @@ st.header("Talk with your data!")
 csv_file = st.file_uploader("Upload a CSV file", type="csv")
 input_text = st.text_input("Ask questions about your data")
 
+if csv_file is not None:
+    agent = create_csv_agent(
+        ChatOpenAI(
+            model="gpt-3.5-turbo"
+        ),
+        csv_file,
+        verbose = True,
+        
+    )
+# ZERO_SHOT_REACT_DESCRIPTION: A zero-shot agent that performs a reasoning step before taking action.
 # openai llm
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 output_parser = StrOutputParser()
