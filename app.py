15f9384aeefb5074691facaab5aeac46d5df84c9
Babak <babak.barghi@conti.de>
2024-04-24 14:38:25 +0200

    ai model as intance

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
 
+# AI model
+llm0 = ChatOpenAI(
+                model="gpt-3.5-turbo",
+                temperature=0
+)
+
 #Define a prompt template for conversation
 prompt = ChatPromptTemplate.from_messages([
     ("system", "As a top-tier data scientist, your role is to derive insightful analyses and patterns from various datasets. You're specialized in marketing and sales analytics, your role is to extract actionable insights and identify customer behavior patterns from sales and marketing campaign datasets. Use your expertise to provide recommendations and strategies that would benefit marketing or sales managers. Highlight key trends, predictive analytics, and segmentation analysis. Your responses should be clear, insightful, and in professional data science terminology. Address specific queries with detailed data insights, trend analysis, and predictive outcomes when applicable. Always provide comprehensive explanations about the steps you went through to get to the Final Answer. Please output a paragraph summurizing you findings"),
@@ -83,10 +89,7 @@ if st.session_state['clicked']:
             st.write(df.head(5))
 
         agent = create_pandas_dataframe_agent(
-            ChatOpenAI(
-                model="gpt-3.5-turbo",
-                temperature=0
-            ),
+            llm0,
             df,
             #io.StringIO(file_content.decode('utf-8', errors='ignore')),
             verbose=True,
